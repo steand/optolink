788f18fcf747aebaddd02c3db0ff20f88de77a51
steand <stefan@myandres.de>
2015-01-15 17:31:27 +0100

    Init

---
+++ src/main/java/de/myandres/optolink/Main.java
@@ -0,0 +1,20 @@
+package de.myandres.optolink;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	
+    static Logger log = LoggerFactory.getLogger(Main.class);
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    
+	    
+	    log.info("Programm gestartet");
+		System.out.println("Hallo");
+		log.debug("Das war es");
+
+	}
+
+}
