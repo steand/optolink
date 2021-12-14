33db25937d121b27693c0bb3774c75e20372a8b7
Leif Bladt <lbladt@testo.de>
2021-12-14 23:59:16 +0100

    Sitch to jSerialComm

--- src/main/java/de/myandres/optolink/Main.java
+++ src/main/java/de/myandres/optolink/Main.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 3.0 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-3.0.html
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -32,7 +32,7 @@ public class Main {
 
 		try {
 
-			// config = new Config("src/main/resources/optolink.xml");
+//			 config = new Config("src/main/resources/optolink.xml");
 			config = new Config("conf/optolink.xml");
 
 			// Init TTY Handling for Optolink
--- src/main/java/de/myandres/optolink/OptolinkInterface.java
+++ src/main/java/de/myandres/optolink/OptolinkInterface.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 3.0 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl-3.0.html
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -13,17 +13,16 @@
  *******************************************************************************/
 package de.myandres.optolink;
 
-import gnu.io.CommPort;
-import gnu.io.CommPortIdentifier;
-import gnu.io.SerialPort;
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortInvalidPortException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class OptolinkInterface {
 
@@ -32,13 +31,13 @@ public class OptolinkInterface {
 	private OutputStream output;
 	private InputStream input;
 	private Config config;
-	private CommPortIdentifier portIdentifier;
-	private CommPort commPort;
+//	private CommPortIdentifier portIdentifier;
+	private SerialPort serialPort;
 	private Socket socket = null;
 
 
-//TODO implement as runnable for URL-based optolinks	
-	
+//TODO implement as runnable for URL-based optolinks
+
 	OptolinkInterface(Config config) throws Exception {
 
 		// constructor with implicit open
@@ -49,23 +48,26 @@ public class OptolinkInterface {
 			log.debug("TTY type URL is present");
 		} else { // device is local
 			log.debug("Open TTY {} ...", this.config.getTTY());
-			portIdentifier = CommPortIdentifier.getPortIdentifier(this.config.getTTY());
-	
-			if (portIdentifier.isCurrentlyOwned()) {
-				log.error("TTY {} in use.", this.config.getTTY());
-				throw new IOException();
-			}
-			commPort = portIdentifier.open(this.getClass().getName(), this.config.getTtyTimeOut());
-			if (commPort instanceof SerialPort) {
-				SerialPort serialPort = (SerialPort) commPort;
-				serialPort.setSerialPortParams(4800, SerialPort.DATABITS_8,
-						SerialPort.STOPBITS_2, SerialPort.PARITY_EVEN);
-	
+			try {
+				serialPort = SerialPort.getCommPort(this.config.getTTY());
+				serialPort.setComPortParameters(4800, 8, 2, SerialPort.EVEN_PARITY);
+
 				input = serialPort.getInputStream();
 				output = serialPort.getOutputStream();
-				commPort.enableReceiveTimeout(this.config.getTtyTimeOut()); // Reading Time-Out
+//			this.serialPort.enableReceiveTimeout(this.config.getTtyTimeOut()); // Reading Time-Out
+				log.debug("TTY {} opened", this.config.getTTY());
+			} catch (SerialPortInvalidPortException e)  {
+				log.error("Can't open serial port", e);
+				throw new IOException();
 			}
-			log.debug("TTY {} opened", this.config.getTTY());
+//			portIdentifier = CommPortIdentifier.getPortIdentifier(this.config.getTTY());
+
+//			if (portIdentifier.isCurrentlyOwned()) {
+//				log.error("TTY {} in use.", this.config.getTTY());
+//				throw new IOException();
+//			}
+//			serialPort = portIdentifier.open(this.getClass().getName(), this.config.getTtyTimeOut());
+//			SerialPort serialPort = this.serialPort;
 		}
 	}
 
@@ -82,7 +84,7 @@ public class OptolinkInterface {
 			}
 		} else {
 			log.debug("Close TTY {} ....", this.config.getTTY());
-			commPort.close();
+			serialPort.closePort();
 			log.debug("TTY {} closed", this.config.getTTY());
 		}
 	}
@@ -101,8 +103,8 @@ public class OptolinkInterface {
 	public synchronized void flush() {
 		// Flush input Buffer
 		if (this.config.getTTYType().matches("URL")) {
-			// We have to wait a certain time. It seems that input.available always has the count 0 
-			// right after connecting 
+			// We have to wait a certain time. It seems that input.available always has the count 0
+			// right after connecting
 			try {
 				Thread.sleep(30); // 10 ms is too low. 30 ms chosen for a certain fail safe distance
 			} catch (InterruptedException e) {
@@ -142,7 +144,7 @@ public class OptolinkInterface {
 		return -1; // Ups
 
 	}
-	
+
 	public String getDeviceName() {
 		return this.config.getDeviceType();
 	}
